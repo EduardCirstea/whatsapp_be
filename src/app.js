432ddb319e919ea6c8380e0ccb438c79fa58bb88
Eduard <eduard_e28@yahoo.com>
2024-07-09 13:26:14 +0300

    routing and mongodb connect

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import fileUpload from "express-fileupload";
 import cors from "cors";
-
+import createHttpError from "http-errors";
+import routes from "./routes/index.js";
 // dotEnv config
 dotenv.config();
 
@@ -46,8 +47,21 @@ app.use(
   // { origin: "http://localhost:3000" } - we are in development
 );
 
-app.post("/", (req, res) => {
-  res.send(req.body);
+//API v1 routes
+app.use("/api/v1", routes);
+
+//Error handling
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist"));
+});
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
+import mongoose from "mongoose";
 import app from "./app.js";
 import logger from "./configs/logger.config.js";
 
 //env variables
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+//exit on mongodb error
+mongoose.connection.on("error", (e) => {
+  logger.error(`MongoDb connection error:${e}`);
+  process.exit(1);
+});
+
+// mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+//MONGODB connection
+mongoose.connect(DATABASE_URL).then(() => {
+  logger.info("Connected to MongoDb");
+});
+
+//starting the server
+let server;
+server = app.listen(PORT, () => {
   logger.info(`Server listening on port: ${PORT}`);
+});
+
+//handle server erros
+
+const exitHandler = () => {
+  if (server) {
+    logger.info(`Server closed.`);
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info(`Server closed.`);
+    process.exit(1);
+  }
 });

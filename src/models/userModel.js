b095af6e66ff2e6cd859eb5e256fde0e21591239
Eduard <eduard_e28@yahoo.com>
2024-07-12 00:10:10 +0300

    some minor changes

---
+++ src/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+    },
+    email: {
+      type: String,
+      required: [true, " Please provide your email adress"],
+      unique: [true, "This email adress already exist"],
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email address"],
+    },
+    picture: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dkd5jblv5/image/upload/v1675976806/Default_ProfilePicture_gjngnb.png",
+    },
+    status: {
+      type: String,
+      default: "Hey there ! I am using whatsapp",
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minLength: [6, "Please make sure is at least 6 character long"],
+      maxLength: [128, "please make sure is less than 128 characters"],
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+const UserModel =
+  mongoose.models.UserModel || mongoose.model("UserModel", userSchema);
+export default UserModel;

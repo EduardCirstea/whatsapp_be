648b38feed80bcae4ac5ba07534efb036740910b
Eduard <eduard_e28@yahoo.com>
2024-07-17 15:45:07 +0300

    auth done in backend

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
+import createHttpError from "http-errors";
 import { createUser, signUser } from "../services/auth.services.js";
-import { generateToken } from "../services/token.service.js";
+import { generateToken, verifyToken } from "../services/token.service.js";
+import { findUser } from "../services/user.services.js";
 
 export const register = async (req, res, next) => {
   try {
@@ -69,7 +71,7 @@ export const login = async (req, res, next) => {
 
     console.table({ access_token, refresh_token });
     res.json({
-      message: "register success",
+      message: "loggin success",
       access_token,
       user: {
         _id: user._id,
@@ -97,6 +99,28 @@ export const logout = async (req, res, next) => {
 
 export const refreshToken = async (req, res, next) => {
   try {
+    const refresh_token = req.cookies.refreshtoken;
+    if (!refresh_token) throw createHttpError.Unauthorized("Please login");
+    const check = await verifyToken(
+      refresh_token,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const user = await findUser(check.userId);
+    const access_token = await generateToken(
+      { userId: user._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    res.json({
+      access_token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        status: user.status,
+        picture: user.picture,
+      },
+    });
   } catch (error) {
     next(error);
   }

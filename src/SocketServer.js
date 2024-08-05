f44337d941a8eccb2292e210ac9e06cde4a28413
Eduard <eduard_e28@yahoo.com>
2024-08-05 19:34:56 +0300

    adding send and receive messages socket

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -12,12 +12,11 @@ export default function (socket) {
 
   //send/receive message
   socket.on("send message", (message) => {
-    console.log("new msg ----", message);
     let conversation = message.conversation;
     if (!conversation.users) return;
     conversation.users.forEach((user) => {
       if (user._id === message.sender._id) return;
-      socket.in(user._id).emit("message received", message);
+      socket.in(user._id).emit("receive message", message);
     });
   });
 }

a59d9bdf2e922fb80ba32666a69258e1812c4731
Eduard <eduard_e28@yahoo.com>
2024-08-06 14:51:25 +0300

    for online status and offline status

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -1,8 +1,23 @@
-export default function (socket) {
+let onlineUsers = [];
+export default function (socket, io) {
   //user joins or opens the application
   socket.on("join", (user) => {
     socket.join(user);
+    //add users to online users
+    if (!onlineUsers.some((u) => u.userId === user)) {
+      console.log(`user ${user} is now online`);
+      onlineUsers.push({ userId: user, socketId: socket.id });
+    }
+    //send online users to client side
+    io.emit("get-online-users", onlineUsers);
   });
+
+  // socket disconnected
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    io.emit("get-online-users", onlineUsers);
+  });
+
   //join a conversation room
   socket.on("join conversation", (conversation) => {
     socket.join(conversation);
--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   logger.info("Socket io connected succesfully");
-  SocketServer(socket);
+  SocketServer(socket, io);
 });
 
 const exitHandler = () => {
